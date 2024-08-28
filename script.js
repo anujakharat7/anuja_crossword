5a7b719c72fc4a5cb2b0b5b402de76283c3357f3
anujakharat7 <147605540+anujakharat7@users.noreply.github.com>
2024-08-28 22:42:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 		// two entries: an across entry and a down entry
 		var puzzleData = [
 			 	{
-					clue: "Imagine you are part of a group full of energy and new ideas, always bringing change. What word describes this group?",
-					answer: "youth",
+					clue: "A tiny light that starts a festival firework",
+					answer: "spark",
 					position: 1,
 					orientation: "across",
 					startx: 1,
@@ -22,55 +22,55 @@
 					starty: 1
 				},
 				{
-					clue: "Imagine a world where everything is always moving forward—what word describes this kind of change?",
-					answer: "progressive",
+					clue: "An artist's crowning achievement",
+					answer: "masterpiece",
 					position: 5,
 					orientation: "across",
 					startx: 1,
 					starty: 3
 				},
 				{
-					clue: "It's what happens when creativity meets progress.",
-					answer: "innovation",
+					clue: "A sense of fulfilment or joy",
+					answer: "happiness",
 					position: 8,
 					orientation: "across",
 					startx: 1,
 					starty: 5
 				},
 				{
-					clue: "When success and wealth are thriving, what’s the term for it?",
-					answer: "prosperity",
+					clue: "Talent for making something unique",
+					answer: "creativity",
 					position: 10,
 					orientation: "across",	
 					startx: 2,
 					starty: 7
 				},
 				{
-					clue: "What do you call the gradual improvement and advancement of something?",
-					answer: "development",
+					clue: "Joyful event with festivities and enjoyment",
+					answer: "celebration",
 					position: 13,
 					orientation: "across",
 					startx: 1,
 					starty: 9
 				},
 				{
-					clue: "What term describes city areas that are bustling with growth and change?",
-					answer: "urban",
+					clue: "Platform that allows you to store and share files online",
+					answer: "cloud",
 					position: 16,
 					orientation: "across",
 					startx: 1,
 					starty: 11
 				},
 				{
-					clue: "Represents the backbone of agriculture and traditional livelihoods.",
-					answer: "rural",
+					clue: "Sandy Spot where you find seashells",
+					answer: "beach",
 					position: 17,
 					orientation: "across",
 					startx: 7,
 					starty: 11
 				},
 				{
-					clue: "Defines the alpha-numeric characters that are typically associated with text used in programming",
+					clue: "Code used to represent text in computers",
 					answer: "ascii",
 					position: 1,
 					orientation: "down",
@@ -78,7 +78,7 @@
 					starty: 1
 				},
 				{
-					clue: "In programming, what’s the term for a structure where each element is placed next to the previous one?",
+					clue: "List that stores multiple items in one place in programming",
 					answer: "array",
 					position: 2,
 					orientation: "down",
@@ -86,16 +86,16 @@
 					starty: 1
 				},
 				{
-					clue: "This acronym stands for a set of instructions for building software. What is it?",
-					answer: "api",
+					clue: "State of extreme happiness",
+					answer: "joy",
 					position: 4,
 					orientation: "down",
 					startx: 9,
 					starty: 1
 				},
 				{
-					clue: "Think of the abbreviation for a platform that helps you code, debug, and manage projects. What is it?",
-					answer: "ide",
+					clue: "Bright object in the sky during the day",
+					answer: "sun",
 					position: 6,
 					orientation: "down",
 					startx: 7,
@@ -118,15 +118,15 @@
 					starty: 5
 				},
 				{
-					clue: "What do you call the quality of working together in harmony and cooperation?",
-					answer: "unity",
+					clue: "A giant sea creature that sings songs underwater.",
+					answer: "whale",
 					position: 11,
 					orientation: "down",
 					startx: 11,
 					starty: 7
 				},
 				{
-					clue: "Whats the short form for the software that manages a computer's hardware and software resources?",
+					clue: "Short form for a system software that manages a computer s hardware and software resources?",
 					answer: "os",
 					position: 12,
 					orientation: "down",
@@ -134,16 +134,16 @@
 					starty: 8
 				},
 				{
-					clue: "Whats the abbreviation for the set of commands used to add, modify, or delete data in a database?",
-					answer: "dml",
+					clue: "Favorite beverage of Indian people",
+					answer: "tea",
 					position: 14,
 					orientation: "down",
 					startx: 3,
 					starty: 9
 				},
 				{
-					clue: "Think of the short form for the protocol that manages data flow and error checking in networking. What is it?",
-					answer: "tcp",
+					clue: "Drawing that helps you find places",
+					answer: "map",
 					position: 15,
 					orientation: "down",
 					startx: 7,
